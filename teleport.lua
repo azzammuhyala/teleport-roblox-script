d4297ae041d53e0fbd5901c671a5a22bec8879fc
azzammuhyala <143919179+azzammuhyala@users.noreply.github.com>
2025-05-18 13:51:46 +0700

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -1,5 +1,5 @@
 -- Teleport Script By Azzam Muhyala
--- Day 2 bikin script sendiri :v
+-- Day 3 bikin script sendiri :v
 
 local players = game.Players
 local localPlayer = players.LocalPlayer
@@ -10,12 +10,12 @@ local clickToTeleportTool = Instance.new("Tool")
 clickToTeleportTool.Name = "Click To Teleport"
 clickToTeleportTool.RequiresHandle = false
 clickToTeleportTool.CanBeDropped = false
-clickToTeleportTool.Activated:Connect(function ()
+clickToTeleportTool.Activated:Connect(function()
     localPlayer.Character.HumanoidRootPart.CFrame = mouse.Hit + Vector3.new(0, 3, 0)
 end)
 
 local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "TeleportToPlayer"
+screenGui.Name = "ZamScriptTeleportToPlayer"
 screenGui.ResetOnSpawn = false
 screenGui.IgnoreGuiInset = true
 screenGui.Parent = localPlayer.PlayerGui
@@ -31,46 +31,73 @@ mainFrame.Parent = screenGui
 
 local headerFrame = Instance.new("Frame")
 headerFrame.Size = UDim2.new(1, 0, 0.15, 0)
-headerFrame.BackgroundColor3 = Color3.fromRGB(84, 84, 84)
+headerFrame.BackgroundTransparency = 1
 headerFrame.LayoutOrder = 1
 headerFrame.Parent = mainFrame
 
-local scrollingFrame = Instance.new("ScrollingFrame")
-scrollingFrame.Size = UDim2.new(1, 0, 0.85, 0)
-scrollingFrame.CanvasSize = UDim2.new(0, 0, 0, 0)
-scrollingFrame.AutomaticCanvasSize = Enum.AutomaticSize.Y
-scrollingFrame.ScrollingDirection = Enum.ScrollingDirection.Y
-scrollingFrame.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
-scrollingFrame.ScrollBarThickness = 0
-scrollingFrame.LayoutOrder = 2
-scrollingFrame.Parent = mainFrame
+local navbarFrame = Instance.new("Frame")
+navbarFrame.Size = UDim2.new(1, 0, 0.1, 0)
+navbarFrame.BackgroundTransparency = 1
+navbarFrame.LayoutOrder = 2
+navbarFrame.Parent = mainFrame
+
+local listPlayersScrollingFrame = Instance.new("ScrollingFrame")
+listPlayersScrollingFrame.Size = UDim2.new(1, 0, 0.75, 0)
+listPlayersScrollingFrame.CanvasSize = UDim2.new(0, 0, 0, 0)
+listPlayersScrollingFrame.AutomaticCanvasSize = Enum.AutomaticSize.Y
+listPlayersScrollingFrame.ScrollingDirection = Enum.ScrollingDirection.Y
+listPlayersScrollingFrame.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+listPlayersScrollingFrame.ScrollBarThickness = 0
+listPlayersScrollingFrame.LayoutOrder = 3
+listPlayersScrollingFrame.Parent = mainFrame
 
 local titleLabel = Instance.new("TextLabel")
 titleLabel.Size = UDim2.new(1, 0, 1, 0)
-titleLabel.Text = "Teleport To Player"
-titleLabel.Font = Enum.Font.SourceSans
-titleLabel.TextScaled = true
-titleLabel.TextWrapped = true
 titleLabel.BackgroundColor3 = Color3.fromRGB(84, 84, 84)
 titleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+titleLabel.Font = Enum.Font.SourceSans
+titleLabel.Text = "Teleport To Player"
+titleLabel.TextScaled = true
+titleLabel.TextWrapped = true
 titleLabel.LayoutOrder = 1
 titleLabel.Parent = headerFrame
 
 local minimizeButton = Instance.new("ImageButton")
 minimizeButton.Size = UDim2.new(1, 0, 1, 0)
 minimizeButton.SizeConstraint = Enum.SizeConstraint.RelativeYY
-minimizeButton.LayoutOrder = 2
-minimizeButton.BackgroundColor3 = Color3.fromRGB(84, 84, 84)
 minimizeButton.Image = "rbxassetid://133258489499035"
+minimizeButton.BackgroundColor3 = Color3.fromRGB(84, 84, 84)
+minimizeButton.LayoutOrder = 2
 minimizeButton.Parent = headerFrame
 
 local closeButton = Instance.new("ImageButton")
 closeButton.Size = UDim2.new(1, 0, 1, 0)
 closeButton.SizeConstraint = Enum.SizeConstraint.RelativeYY
-closeButton.LayoutOrder = 3
 closeButton.Image = "rbxassetid://92186614586776"
 closeButton.BackgroundColor3 = Color3.fromRGB(84, 84, 84)
+closeButton.LayoutOrder = 3
 closeButton.Parent = headerFrame
+
+local searchBar = Instance.new("TextBox")
+searchBar.Size = UDim2.new(1, 0, 1, 0)
+searchBar.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
+searchBar.TextColor3 = Color3.fromRGB(255, 255, 255)
+searchBar.PlaceholderColor3 = Color3.fromRGB(106, 106, 106)
+searchBar.Font = Enum.Font.SourceSans
+searchBar.Text = ""
+searchBar.PlaceholderText = "Search Player"
+searchBar.TextScaled = true
+searchBar.TextWrapped = true
+searchBar.LayoutOrder = 1
+searchBar.Parent = navbarFrame
+
+local cancelSearchButton = Instance.new("ImageButton")
+cancelSearchButton.Size = UDim2.new(1, 0, 1, 0)
+cancelSearchButton.SizeConstraint = Enum.SizeConstraint.RelativeYY
+cancelSearchButton.Image = "rbxassetid://92186614586776"
+cancelSearchButton.BackgroundColor3 = Color3.fromRGB(84, 84, 84)
+cancelSearchButton.LayoutOrder = 2
+cancelSearchButton.Parent = navbarFrame
 
 local mainFrameLayout = Instance.new("UIListLayout")
 mainFrameLayout.SortOrder = Enum.SortOrder.LayoutOrder
@@ -83,13 +110,31 @@ headerFrameLayout.FillDirection = Enum.FillDirection.Horizontal
 headerFrameLayout.HorizontalFlex = Enum.UIFlexAlignment.Fill
 headerFrameLayout.Parent = headerFrame
 
+local navbarFrameLayout = Instance.new("UIListLayout")
+navbarFrameLayout.SortOrder = Enum.SortOrder.LayoutOrder
+navbarFrameLayout.FillDirection = Enum.FillDirection.Horizontal
+navbarFrameLayout.HorizontalFlex = Enum.UIFlexAlignment.Fill
+navbarFrameLayout.Parent = navbarFrame
+
 local scrollingFrameLayout = Instance.new("UIListLayout")
 scrollingFrameLayout.SortOrder = Enum.SortOrder.LayoutOrder
 scrollingFrameLayout.Padding = UDim.new(0, 5)
-scrollingFrameLayout.Parent = scrollingFrame
+scrollingFrameLayout.Parent = listPlayersScrollingFrame
+
+local function matchPlayer(displayName, username)
+    local lowerDisplayName = string.lower(displayName)
+    local lowerUsername = string.lower(username)
+    local lowerKeyword = string.lower(searchBar.Text)
+
+    if string.find(lowerDisplayName, lowerKeyword) or string.find(lowerUsername, lowerKeyword) then
+        return true
+    end
+
+    return false
+end
 
 local function updatePlayers()
-    for _, child in ipairs(scrollingFrame:GetChildren()) do
+    for _, child in ipairs(listPlayersScrollingFrame:GetChildren()) do
         if child:IsA("TextButton") then
             child:Destroy()
         end
@@ -97,26 +142,27 @@ local function updatePlayers()
 
     for _, plr in pairs(players:GetPlayers()) do
 
-        if plr ~= localPlayer then
+        local displayName = plr.DisplayName
+        local username = plr.Name
+
+        if plr ~= localPlayer and matchPlayer(displayName, username) then
             local button = Instance.new("TextButton")
 
             button.Size = UDim2.new(1, 0, 0.2, 0)
-            button.Text = plr.DisplayName .. " (" .. plr.Name .. ")"
             button.TextColor3 = Color3.fromRGB(255, 255, 255)
             button.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
             button.Font = Enum.Font.SourceSans
+            button.Text = displayName .. " (" .. username .. ")"
             button.TextScaled = true
             button.TextWrapped = true
-            button.Parent = scrollingFrame
+            button.Parent = listPlayersScrollingFrame
 
             button.Activated:Connect(function()
                 local targetChar = plr.Character
                 local myChar = localPlayer.Character
-
                 if targetChar and myChar then
                     local targetRoot = targetChar:FindFirstChild("HumanoidRootPart")
                     local myRoot = myChar:FindFirstChild("HumanoidRootPart")
-
                     if targetRoot and myRoot then
                         myRoot.CFrame = targetRoot.CFrame
                     end
@@ -126,11 +172,7 @@ local function updatePlayers()
     end
 end
 
-local function createClickToTeleport()
-    clickToTeleportTool.Parent = localPlayer.Backpack
-end
-
-createClickToTeleport()
+clickToTeleportTool.Parent = localPlayer.Backpack
 updatePlayers()
 
 local updateAdded = players.PlayerAdded:Connect(updatePlayers)
@@ -138,17 +180,34 @@ local updateRemoved = players.PlayerRemoving:Connect(updatePlayers)
 local updateCharAdded = localPlayer.CharacterAdded:Connect(function()
     repeat task.wait() until localPlayer:FindFirstChild("Backpack")
     if not localPlayer.Backpack:FindFirstChild("Click To Teleport") then
-        createClickToTeleport()
+        clickToTeleportTool.Parent = localPlayer.Backpack
     end
 end)
 
+searchBar.Changed:Connect(function(property)
+    if property == "Text" then
+        updatePlayers()
+    end
+end)
+
+cancelSearchButton.Activated:Connect(function()
+    searchBar.Text = ""
+    updatePlayers()
+end)
+
 minimizeButton.Activated:Connect(function()
-    if scrollingFrame.Visible then
-        scrollingFrame.Visible = false
+    if listPlayersScrollingFrame.Visible then
         mainFrame.Active = false
+        navbarFrame.Visible = false
+        listPlayersScrollingFrame.Visible = false
+
+        titleLabel.BackgroundTransparency = 0.5
     else
-        scrollingFrame.Visible = true
         mainFrame.Active = true
+        navbarFrame.Visible = true
+        listPlayersScrollingFrame.Visible = true
+
+        titleLabel.BackgroundTransparency = 0
     end
 end)
 
